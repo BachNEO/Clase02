8dafb2be3d00e0f6577d9609a76e514c26bdbe97
Bach <carlos.bachmann@neo.com.pe>
2026-04-08 16:17:34 -0500

    configuración del deploy V2

--- clase02/main.py
+++ clase02/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:8000"],
+    allow_origins=["*"],  # Permite todos los orígenes en producción
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
@@ -36,3 +36,7 @@ def dame_la_hora():
 app.include_router(servicios_router)
 app.include_router(auth_router)
 app.include_router(mascotas_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
